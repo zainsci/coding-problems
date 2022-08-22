d2241905272c9e1d14b1dba498af266b7340266b
zainsci <60652827+zainsci@users.noreply.github.com>
2022-08-22 13:24:44 +0500

    solve power-of-four

---
+++ power-of-four/main.py
@@ -0,0 +1,39 @@
+#! /bin/python3
+
+
+"""
+source: https://leetcode.com/problems/power-of-four/
+problem: https://leetcode.com/problems/power-of-four/
+site: LeetCode
+"""
+
+
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n == 0:
+            return False
+
+        while n != 1:
+            if n % 4 != 0:
+                return False
+            else:
+                n = n // 4
+
+        return True
+
+
+def main():
+    sol = Solution()
+    print(sol.isPowerOfFour(16))
+    print(sol.isPowerOfFour(5))
+    print(sol.isPowerOfFour(0))
+    print(sol.isPowerOfFour(1))
+    print(sol.isPowerOfFour(2))
+    print(sol.isPowerOfFour(3))
+    print(sol.isPowerOfFour(8))
+    print(sol.isPowerOfFour(9))
+    print(sol.isPowerOfFour(64))
+
+
+if __name__ == '__main__':
+    main()
