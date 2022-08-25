91934cf7ce901a3133de8e2390b62f2a2132d37d
zainsci <60652827+zainsci@users.noreply.github.com>
2022-08-25 23:05:32 +0500

    solve ransom-note

---
+++ ransom-note/main.py
@@ -0,0 +1,32 @@
+#! /bin/python3
+
+
+"""
+source: https://leetcode.com/problems/ransom-note/
+problem: https://leetcode.com/problems/ransom-note/
+site: LeetCode
+"""
+
+
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        ransomNote = list(ransomNote)
+        magazine = list(magazine)
+
+        for letter in ransomNote:
+            if not letter in magazine:
+                return False
+            else:
+                magazine.remove(letter)
+        return True
+
+
+def main():
+    sol = Solution()
+    ransomNote = "aa"
+    magazine = "aab"
+    print(sol.canConstruct(ransomNote, magazine))
+
+
+if __name__ == '__main__':
+    main()
