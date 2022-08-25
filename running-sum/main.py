83f42e3d0516d4212d8eb9c823f78ff4f0f2f183
zainsci <60652827+zainsci@users.noreply.github.com>
2022-08-26 00:53:08 +0500

    solve: running-sum

---
+++ running-sum/main.py
@@ -0,0 +1,43 @@
+#! /bin/python3
+
+"""
+source: https://leetcode.com/problems/running-sum-of-1d-array
+problem: https://leetcode.com/problems/running-sum-of-1d-array
+site: LeetCode
+"""
+
+
+class Solution:
+    # def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum(self, nums):
+        new_arr = []
+        total = 0
+        for i in nums:
+            total += i
+            new_arr.append(total)
+        return new_arr
+
+    # runningSum with Recursion
+    # def runningSum(self, nums: List[int]) -> List[int]:
+    def runningSum_recursion(self, nums):
+        if len(nums) == 1:
+            return nums
+
+        next_val = self.runningSum_recursion(nums[:len(nums)-1])
+        total = 0
+        for i in nums:
+            total += i
+        new_arr = [x for x in next_val]
+        new_arr.append(total)
+        return new_arr
+
+
+def main():
+    sol = Solution()
+    nums = [1, 1, 1, 1, 1]
+    print(sol.runningSum(nums))
+    print(sol.runningSum_recursion(nums))
+
+
+if __name__ == "__main__":
+    main()
