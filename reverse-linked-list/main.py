d81d944c45139bc0e4dbd2e941e6fa6c224a436c
zainsci <60652827+zainsci@users.noreply.github.com>
2022-08-27 20:44:36 +0500

    solve reverse-linked-list

---
+++ reverse-linked-list/main.py
@@ -0,0 +1,35 @@
+#! /bin/python3
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    # def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    def reverseList(self, head):
+        new_list = None
+
+        while head:
+            this_node = head
+            head = head.next
+            this_node.next = new_list
+            new_list = this_node
+
+        return new_list
+
+
+def main():
+    sol = Solution()
+    head = ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5)))))
+    new_list = sol.reverseList(head)
+
+    while new_list:
+        print(new_list.val)
+        new_list = new_list.next
+
+
+if __name__ == "__main__":
+    main()
