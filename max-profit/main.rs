b47e01e65ab009705e03edfb3dd09a5dc8d6aa77
zainsci <60652827+zainsci@users.noreply.github.com>
2022-08-29 15:03:30 +0500

    solve max-profit.rs

--- max-profit/main.rs
+++ max-profit/main.rs
@@ -1,8 +1,26 @@
-impl Solution {
-    pub fn max_profit(prices: Vec<i32>) -> i32 {}
+fn max_profit(prices: &[i32]) -> i32 {
+    let mut min_price = 9999;
+    let mut max_profit = 0;
+    let mut i = prices.len() - 1;
+
+    loop {
+        if i == 0 {
+            break;
+        }
+
+        if min_price > prices[i] {
+            min_price = prices[i];
+        } else if prices[i] - min_price > max_profit {
+            max_profit = prices[i] - min_price;
+        }
+        i = i - 1;
+    }
+
+    return max_profit;
 }
 
 fn main() {
     let prices = [7, 6, 4, 3, 2, 1];
-    let sol = Solution();
+    let sol = max_profit(&prices);
+    println!("{sol}");
 }
