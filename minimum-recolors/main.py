e09af749d3e8c22c825bd112bd3b3ad29c474506
zainsci <60652827+zainsci@users.noreply.github.com>
2024-06-30 23:57:57 +0300

    solve minimum-recolors with sliding-window-technique

--- minimum-recolors/main.py
+++ minimum-recolors/main.py
@@ -1,5 +1,5 @@
 #! /bin/python3
-from collections import Counter
+# from collections import Counter
 
 """
 source: https://leetcode.com/problems/minimum-recolors-to-get-k-consecutive-black-blocks/
@@ -9,17 +9,40 @@ site: LeetCode
 """
 
 
+# Works but Slow
+# class Solution:
+#     def minimumRecolors(self, blocks, k):
+#         min_ops = 1000
+#
+#         for i in range(len(blocks)-k+1):
+#             block_count = Counter(blocks[i:i+k])
+#
+#             if block_count["W"] < min_ops:
+#                 min_ops = block_count["W"]
+#
+#         return min_ops
+
+
 class Solution:
+    # def minimumRecolors(self, blocks: str, k: int) -> int:
     def minimumRecolors(self, blocks, k):
-        min_ops = 1000
+        min_ops = 0
+        ans = 0
 
-        for i in range(len(blocks)-k+1):
-            block_count = Counter(blocks[i:i+k])
+        for i in range(k):
+            if blocks[i] == "W":
+                min_ops += 1
+                ans = min_ops
 
-            if block_count["W"] < min_ops:
-                min_ops = block_count["W"]
+        for i in range(k, len(blocks)):
+            if blocks[i] == "W":
+                min_ops += 1
+            if blocks[i-k] == "W":
+                min_ops -= 1
 
-        return min_ops
+            ans = min(ans, min_ops, )
+
+        return ans
 
 
 def main():
