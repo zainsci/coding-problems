b38bc55fe76369cb1169e9bbd90d0863ed016872
zainsci <60652827+zainsci@users.noreply.github.com>
2024-06-30 23:34:43 +0300

    solve max-vowels-substring with Sliding Window Technique

--- max-vowels-substring/main.py
+++ max-vowels-substring/main.py
@@ -8,16 +8,40 @@ site: LeetCode
 """
 
 
+# LeetCode: Time Limit Exceeded
+# class Solution:
+#     def maxVowels(self, s, k):
+#         VOWELS = {"a", "e", "i", "o", "u"}
+#         max_len = 0
+#
+#         for i in range(len(s)-k+1):
+#             count = [1 if x in VOWELS else 0 for x in s[i:i+k]]
+#
+#             if sum(count) > max_len:
+#                 max_len = sum(count)
+#
+#         return max_len
+
+
+# https://stackoverflow.com/questions/8269916/what-is-sliding-window-algorithm-examples
+# learned about Sliding Window Approach
 class Solution:
     def maxVowels(self, s, k):
         VOWELS = {"a", "e", "i", "o", "u"}
+        curr_sum = 0
         max_len = 0
 
-        for i in range(len(s)-k+1):
-            count = [1 if x in VOWELS else 0 for x in s[i:i+k]]
+        for i in range(k):
+            curr_sum += 1 if s[i] in VOWELS else 0
+            max_len = curr_sum
 
-            if sum(count) > max_len:
-                max_len = sum(count)
+        for i in range(k, len(s)):
+            if s[i] in VOWELS:
+                curr_sum += 1
+            if s[i-k] in VOWELS:
+                curr_sum -= 1
+
+            max_len = max(max_len, curr_sum)
 
         return max_len
 
