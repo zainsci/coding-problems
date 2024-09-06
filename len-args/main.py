81cb430394a02d51517d7408330ba578ec913e7e
zainsci <60652827+zainsci@users.noreply.github.com>
2024-09-06 11:11:20 +0300

    solve len-args

---
+++ len-args/main.py
@@ -0,0 +1,16 @@
+#! /bin/python3
+
+"""
+source: https://leetcode.com/problems/return-length-of-arguments-passed/
+problem: https://leetcode.com/problems/return-length-of-arguments-passed/
+type: Easy
+site: LeetCode
+"""
+
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()
