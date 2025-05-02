e6428065cbd30c1f65d17b4a8de4cebeb9916449
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-02 22:18:13 +0300

    solve reverse-degree-str

---
+++ reverse-degree-str/main.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+"""
+source: https://leetcode.com/problems/reverse-degree-of-a-string/description/
+problem: https://leetcode.com/problems/reverse-degree-of-a-string/description/
+type: Easy
+site: LeetCode
+submission: https://leetcode.com/problems/reverse-degree-of-a-string/submissions/1623880816/
+"""
+
+
+class Solution:
+    def reverseDegree(self, s: str) -> int:
+        idxs = {key: 26 - val for val,
+                key in enumerate("abcdefghijklmnopqrstuvwxyz")}
+
+        total = 0
+        for x, y in enumerate(s):
+
+            total += idxs[y] * (x+1)
+        return total
+
+
+def main():
+    sol = Solution()
+
+    s = "abc"
+    print(sol.reverseDegree(s))
+
+    s = "zaza"
+    print(sol.reverseDegree(s))
+    pass
+
+
+if __name__ == "__main__":
+    main()
