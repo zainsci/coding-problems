f281b885a02f2eb35f3b2b713f101f658f0fe1dd
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-06 23:12:30 +0300

    optimize elem-sum-dig-sum

--- elem-sum-dig-sum/main.py
+++ elem-sum-dig-sum/main.py
@@ -5,7 +5,7 @@ source: https://leetcode.com/problems/difference-between-element-sum-and-digit-s
 problem: https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/
 type: Easy
 site: LeetCode
-submission: https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/submissions/1627313011/
+submission: https://leetcode.com/problems/difference-between-element-sum-and-digit-sum-of-an-array/submissions/1627314778/
 """
 
 
@@ -13,7 +13,15 @@ class Solution:
     # def differenceOfSum(self, nums: List[int]) -> int:
     def differenceOfSum(self, nums):
         elem_sum = sum(nums)
-        dig_sum = sum([sum([int(x) for x in str(y)]) for y in nums])
+        dig_sum = 0
+
+        for i in nums:
+            if i <= 9:
+                dig_sum += i
+            else:
+                for j in str(i):
+                    dig_sum += int(j)
+
         return abs(elem_sum-dig_sum)
 
 
