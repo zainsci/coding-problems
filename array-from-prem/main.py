720fa9a9bbde4153bfc191fb056d6b09e1b9ddbb
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-06 14:40:18 +0300

    optimize array-from-perm

--- array-from-prem/main.py
+++ array-from-prem/main.py
@@ -5,29 +5,32 @@ source: https://leetcode.com/problems/build-array-from-permutation
 problem: https://leetcode.com/problems/build-array-from-permutation
 type: easy
 site: LeetCode
+submission: https://leetcode.com/problems/build-array-from-permutation/submissions/1626939893/
 """
+
 
 class Solution:
     # def buildArray(self, nums: List[int]) -> List[int]:
-    def buildArray(self, nums) :
-      new_arr = [None for _ in nums]
-      
-      for i in range(len(nums)):
-        new_arr[i] = nums[nums[i]]
+    def buildArray(self, nums):
+        new_arr = []
 
-      return new_arr
+        for i in range(len(nums)):
+            new_arr.append(nums[nums[i]])
+
+        return new_arr
+
 
 def main():
-  sol = Solution()
+    sol = Solution()
 
-  nums = [0,2,1,5,3,4]
-  print(sol.buildArray(nums))
-  
-  nums = [5,0,1,2,3,4]
-  print(sol.buildArray(nums))
+    nums = [0, 2, 1, 5, 3, 4]
+    print(sol.buildArray(nums))
 
-  return 
+    nums = [5, 0, 1, 2, 3, 4]
+    print(sol.buildArray(nums))
+
+    return
 
 
-if __name__ == "__main__": 
-  main()
+if __name__ == "__main__":
+    main()
