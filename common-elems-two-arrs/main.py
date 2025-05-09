d6eff7726974a51d1a4846498de1bbc5889ac5a0
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-09 18:13:18 +0300

    optimize common-elems-two-arrs

--- common-elems-two-arrs/main.py
+++ common-elems-two-arrs/main.py
@@ -5,26 +5,19 @@ source: https://leetcode.com/problems/find-common-elements-between-two-arrays/
 problem: https://leetcode.com/problems/find-common-elements-between-two-arrays/
 type: Easy
 site: LeetCode
-submission: https://leetcode.com/problems/find-common-elements-between-two-arrays/submissions/1629319574/
+submission: https://leetcode.com/problems/find-common-elements-between-two-arrays/submissions/1629391312/
 """
 
 
 class Solution:
     # def findIntersectionValues(self, nums1: List[int], nums2: List[int]) -> List[int]:
     def findIntersectionValues(self, nums1, nums2):
-        count_1 = 0
-        count_2 = 0
 
         nums1_set = set(nums1)
         nums2_set = set(nums2)
 
-        for i in nums1:
-            if i in nums2_set:
-                count_1 += 1
-
-        for i in nums2:
-            if i in nums1_set:
-                count_2 += 1
+        count_1 = sum([1 for x in nums1 if x in nums2_set])
+        count_2 = sum([1 for x in nums2 if x in nums1_set])
 
         return [count_1,  count_2]
 
