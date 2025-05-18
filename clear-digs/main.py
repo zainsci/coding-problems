f688ad6fb2d79d5c4aafe48aed54fa45d523a15e
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-18 20:23:46 +0300

    optimize clear-digs

--- clear-digs/main.py
+++ clear-digs/main.py
@@ -5,30 +5,22 @@ source: https://leetcode.com/problems/clear-digits/
 problem: https://leetcode.com/problems/clear-digits/
 type: Easy
 site: LeetCode
-submission: https://leetcode.com/problems/clear-digits/submissions/1637491687/
+submission: https://leetcode.com/problems/clear-digits/submissions/1637497868/
 """
 
 
 class Solution:
     # def clearDigits(self, s: str) -> str:
     def clearDigits(self, s):
-        s = list(s)
-        n = 0
+        ans = []
 
-        while n < len(s):
-            char = s[n]
+        for i in range(len(s)):
+            if s[i].isdigit():
+                ans.pop()
+            else:
+                ans.append(s[i])
 
-            try:
-                _ = int(char)
-                s.pop(n)
-                s.pop(n-1)
-                n -= 1
-
-            except ValueError:
-                n += 1
-                continue
-
-        return "".join(s)
+        return "".join(ans)
 
 
 def main():
