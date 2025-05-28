96d1a591862e9360a5c6b92be9374ba59e4ab80f
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-28 17:20:01 +0300

    optimize lemonade-change

--- lemonade-change/main.py
+++ lemonade-change/main.py
@@ -7,61 +7,54 @@ source: https://leetcode.com/problems/lemonade-change/
 problem: https://leetcode.com/problems/lemonade-change/
 type: Easy
 site: LeetCode
-submission: https://leetcode.com/problems/lemonade-change/submissions/1646929693/
+submission: https://leetcode.com/problems/lemonade-change/submissions/1647067554/
 """
 
 
 class Solution:
     # def lemonadeChange(self, bills: List[int]) -> bool:
     def lemonadeChange(self, bills):
-        change = deque([])
+        change = {5: 0, 10: 0, 20: 0}
 
         for bill in bills:
-            if bill == 5:
-                change.appendleft(5)
+            change[bill] += 1
 
-            else:
-                if len(change) == 0:
+            if bill == 10:
+                if change[5] <= 0:
                     return False
+                change[5] -= 1
 
-                elif bill == 10:
-                    print(change)
-                    change.popleft()
-                    change.append(10)
-                else:
-                    print(change)
-                    if min(change) != 5:
+            elif bill == 20:
+                if change[10] <= 0:
+                    if change[5] <= 2:
                         return False
-                    if sum(change) < 15:
-                        return False
-                    if max(change) != 10:
-                        change.popleft()
-                        change.popleft()
-                        change.popleft()
                     else:
-                        change.popleft()
-                        change.pop()
+                        change[5] -= 3
+                elif change[5] == 0:
+                    return False
+                else:
+                    change[5] -= 1
+                    change[10] -= 1
 
         return True
 
 
 def main():
     sol = Solution()
-    # bills = [5, 5, 5, 10, 20]
-    # print(sol.lemonadeChange(bills))
-
-    # bills = [5, 5, 10, 10, 20]
-    # print(sol.lemonadeChange(bills))
-
-    # bills = [10, 10]
-    # print(sol.lemonadeChange(bills))
-
-    # bills = [5, 5, 5, 10, 5, 5, 10, 20, 20, 20]
-    # print(sol.lemonadeChange(bills))
-
-    bills = [5, 5, 5, 20, 5, 5, 5, 20, 5, 5,
-             5, 10, 5, 20, 10, 20, 10, 20, 5, 5]
+    bills = [5, 5, 5, 10, 20]
     print(sol.lemonadeChange(bills))
+
+    bills = [5, 5, 10, 10, 20]
+    print(sol.lemonadeChange(bills))
+
+    bills = [10, 10]
+    print(sol.lemonadeChange(bills))
+
+    bills = [5, 5, 5, 10, 5, 5, 10, 20, 20, 20]
+    print(sol.lemonadeChange(bills))
+
+    bills = [5, 5, 10, 20, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 5, 5, 20, 5, 20, 5]
+    print(sol.lemonadeChange(bills))  # True
 
     pass
 
