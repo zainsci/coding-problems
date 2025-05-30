f01b99247fa35dcbf4997fc50ddb0ac7b579ec01
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-30 16:41:46 +0300

    solve reverse-polish-notation

---
+++ reverse-polish-notation/main.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+"""
+source: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+problem: https://leetcode.com/problems/evaluate-reverse-polish-notation/
+type: Medium
+site: LeetCode
+submission: https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/1648927972/
+"""
+
+
+class Solution:
+    # def evalRPN(self, tokens: List[str]) -> int:
+    def evalRPN(self, tokens):
+        stack = []
+
+        for token in tokens:
+            try:
+                num = int(token)
+                stack.append(num)
+            except ValueError:
+                if token == "+":
+                    a = stack.pop()
+                    b = stack.pop()
+                    stack.append(b + a)
+
+                if token == "-":
+                    a = stack.pop()
+                    b = stack.pop()
+                    stack.append(b - a)
+
+                if token == "*":
+                    a = stack.pop()
+                    b = stack.pop()
+                    stack.append(b * a)
+
+                if token == "/":
+                    a = stack.pop()
+                    b = stack.pop()
+                    stack.append(int(b / a))
+
+        return stack[0]
+
+
+def main():
+    sol = Solution()
+
+    tokens = ["2", "1", "+", "3", "*"]
+    print(sol.evalRPN(tokens))
+
+    tokens = ["4", "13", "5", "/", "+"]
+    print(sol.evalRPN(tokens))
+
+    tokens = ["10", "6", "9", "3", "+", "-11",
+              "*", "/", "*", "17", "+", "5", "+"]
+    print(sol.evalRPN(tokens))
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
