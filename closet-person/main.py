2290647da55f344a5fde3040cb8c79eab21be503
zainsci <60652827+zainsci@users.noreply.github.com>
2025-05-30 10:29:50 +0300

    one-liner closet-person

--- closet-person/main.py
+++ closet-person/main.py
@@ -5,17 +5,14 @@ source: https://leetcode.com/problems/find-closest-person/
 problem: https://leetcode.com/problems/find-closest-person/
 type: Easy
 site: LeetCode
-submission: https://leetcode.com/problems/find-closest-person/submissions/1648670123/
+submission: https://leetcode.com/problems/find-closest-person/submissions/1648673111/
 """
 
 
 class Solution:
     # def findClosest(self, x: int, y: int, z: int) -> int:
     def findClosest(self, x, y, z):
-        d1 = abs(x - z)
-        d2 = abs(y - z)
-
-        return 1 if d1 < d2 else 2 if d1 > d2 else 0
+        return 1 if abs(x - z) < abs(y - z) else 2 if abs(x - z) > abs(y - z) else 0
 
 
 def main():
